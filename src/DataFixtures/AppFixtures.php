00c4acc8057d10c62fbdbf916502c449c11a0a51
unknown <kousay.najar@esprit.tn>
2024-09-04 12:16:16 +0100

    2 eme pas

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Generator;
+use Faker\Factory;
+use App\Entity\Ingridient;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+
+class AppFixtures extends Fixture
+{
+   /**
+    * @var Generator
+    */
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker=Factory::create('fr_FR');
+    }
+
+
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        for ($i=0; $i < 50 ; $i++) { 
+            $ingridient= new Ingridient();
+            $ingridient->setName($this->faker->word())
+                ->setPrice(3.0);
+        $manager->persist($ingridient);
+        }
+        
+        $manager->flush();
+    }
+}
